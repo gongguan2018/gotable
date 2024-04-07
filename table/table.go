28d84da685795bd939cc4e2b6f287e06d9b0a162
gongguan2018 <1542345123@qq.com>
2024-04-07 18:08:35 +0800

    modify alignment mode function

--- table/table.go
+++ table/table.go
@@ -8,7 +8,7 @@ import (
 
 type Table struct {
 	tableHeader []string //表头内容
-	maxlen      []string //表头和内容对应索引位置的值的最大长度组成的新切片,后续使用此长度创建表的宽度,这样才会对齐
+	//	maxlen      []string //表头和内容对应索引位置的值的最大长度组成的新切片,后续使用此长度创建表的宽度,这样才会对齐
 	columnName  []string //表列名
 	columnColor string   //列的颜色
 	alignment   int      //对齐方式
@@ -25,22 +25,33 @@ func InitTable(tableHeader []string) *Table {
 		return &Table{}
 	}
 }
+func (t *Table) SetAlignmentMode(alignment ...int) {
+	if len(alignment) == 0 {
+		t.alignment = 2
+	} else if len(alignment) == 1 {
+		align := checkColumnAlignment(alignment[0])
+		if align == 5 {
 
-// 设置对齐方式
-func (t *Table) SetAlignment(alignment int, columnName ...string) {
+			fmt.Println("Error:", "Alignment parameter error,Can only select 0,1,2")
+			os.Exit(-1)
+		} else {
+			t.alignment = align
+		}
+
+	} else {
+               fmt.Println("Error:","Only one parameter can be entered,Can only select 0,1,2")
+	       os.Exit(-1)
+	}
+}
+
+// 设置要对齐的列
+func (t *Table) SetAlignmentColumn(columnName ...string) {
 	if len(t.tableHeader) != 0 {
 		var columnNameSlice []string
 		//如果要设置的列名为空,那么默认就是设置全部列名(不定参数的columnName实际为切片)
 		if len(columnName) == 0 {
-			align := checkColumnAlignment(alignment)
-			if align == 5 {
-				fmt.Println("Alignment parameter error,Can only select 0,1,2")
-				return
-			} else {
-				t.alignment = align
-				columnNameSlice = append(columnNameSlice, "all")
-				t.columnName = columnNameSlice
-			}
+			columnNameSlice = append(columnNameSlice, "all")
+			t.columnName = columnNameSlice
 
 		} else {
 			/*
@@ -50,16 +61,8 @@ func (t *Table) SetAlignment(alignment int, columnName ...string) {
 			b := checkColumnName(columnName)
 			//如果b为true,说明columnName中是包含all的,因此视为设置全部的列
 			if b {
-				align := checkColumnAlignment(alignment)
-				if align == 5 {
-					fmt.Println("Alignment parameter error,Can only select 0,1,2")
-					return
-				} else {
-					t.alignment = align
-					columnNameSlice = append(columnNameSlice, "all")
-					t.columnName = columnNameSlice
-
-				}
+				columnNameSlice = append(columnNameSlice, "all")
+				t.columnName = columnNameSlice
 
 			} else {
 				//比较要设置的列名和表头中的列,是否有输入错误,返回包含错误列的切片
@@ -69,14 +72,7 @@ func (t *Table) SetAlignment(alignment int, columnName ...string) {
 					os.Exit(-1)
 				} else {
 					//len(s)等于0,此时要设置的列是正确的,开始设置
-					align := checkColumnAlignment(alignment)
-					if align == 5 {
-						fmt.Println("Alignment parameter error,Can only select 0,1,2")
-						return
-					} else {
-						t.alignment = align
-						t.columnName = columnName
-					}
+					t.columnName = columnName
 
 				}
 			}
@@ -99,8 +95,8 @@ func (t *Table) CreateTable(tableheader []string, row [][]string) {
 		t.createTableHeader(tableheader)
 	} else {
 		//取tableheader和row中切片的相同索引的值的最大长度
-	//	maxLengthSlice := t.maxlength(tableheader, row)
-	//	t.maxlen = maxLengthSlice
+		//	maxLengthSlice := t.maxlength(tableheader, row)
+		//	t.maxlen = maxLengthSlice
 		t.CreateTableRow(tableheader, row)
 	}
 }
@@ -109,10 +105,10 @@ func (t *Table) CreateTable(tableheader []string, row [][]string) {
 // 定义二维切片,然后把表头的切片和列切片都添加进去
 func (t *Table) CreateTableRow(tableHeader []string, row [][]string) {
 	/*
-              获取要设置的列在tableHeader中索引,为什么这个调用要放在这里呢,如果放在setSpecifyColumnAlignment函数或者后面的调用
-	      中,由于需求,在后面的调用(croppSlice函数)有设置tableHeader为空,当tableHeader为空的时候,获取到的columnIndex也为空
-	      当我们设置表中多个列向左对齐的时候,不只是表头向左对齐,内容也要向左对齐,此时是根据索引来设置的,表头和表内容的相应
-	      位置索引一样,因此必须保证columnIndex的内容全局不可变,否则可能出现表头左对齐了,但是内容还是居中的情况
+		              获取要设置的列在tableHeader中索引,为什么这个调用要放在这里呢,如果放在setSpecifyColumnAlignment函数或者后面的调用
+			      中,由于需求,在后面的调用(croppSlice函数)有设置tableHeader为空,当tableHeader为空的时候,获取到的columnIndex也为空
+			      当我们设置表中多个列向左对齐的时候,不只是表头向左对齐,内容也要向左对齐,此时是根据索引来设置的,表头和表内容的相应
+			      位置索引一样,因此必须保证columnIndex的内容全局不可变,否则可能出现表头左对齐了,但是内容还是居中的情况
 	*/
 	columnIndex := checkSliceIndex(t.columnName, tableHeader)
 	//二维切片,把表头切片和内容切片都添加进来
@@ -141,7 +137,7 @@ func (t *Table) CreateTableRow(tableHeader []string, row [][]string) {
 				setAllColumnAlignment(t.alignment, totalSlice[i])
 			} else {
 				//给指定的列设置对齐方式
-				setSpecifyColumnAlignment(t.alignment,  totalSlice[i],columnIndex)
+				setSpecifyColumnAlignment(t.alignment, totalSlice[i], columnIndex)
 			}
 
 		}
@@ -155,12 +151,12 @@ func (t *Table) CreateTableRow(tableHeader []string, row [][]string) {
 	第一轮循环后将比较结果依旧赋值给tableheader,这样tableheader的切片就变成了第一次比较后的结果,然后再次进行比较
 	即使row有无数个切片，最终也会获取出最大值
 */
-func (t *Table) maxlength(tableheader []string, row [][]string) []string {
-	for _, rowslice := range row {
-		tableheader = sliceCompare(tableheader, rowslice) //比较结果依旧赋值给tableheader,然后进行下一轮
-	}
-	return tableheader
-}
+//func (t *Table) maxlength(tableheader []string, row [][]string) []string {
+//	for _, rowslice := range row {
+//		tableheader = sliceCompare(tableheader, rowslice) //比较结果依旧赋值给tableheader,然后进行下一轮
+//	}
+//	return tableheader
+//}
 
 // 创建表头,在没有表内容只有表头的时候调用此函数
 func (t *Table) createTableHeader(tableHeader []string) {
